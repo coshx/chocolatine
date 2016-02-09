039348d0fc15d76f9d5a8d1ad0c6a885fc5649ee
Adrien Cadet <acadet@live.fr>
2016-02-09 12:01:57 -0800

    Adds promises

---
+++ base/src/main/java/com/coshx/chocolatine/utils/Promise.java
@@ -0,0 +1,62 @@
+package com.coshx.chocolatine.utils;
+
+import android.util.Pair;
+
+import com.coshx.chocolatine.utils.actions.Action;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Promise
+ * <p/>
+ */
+public class Promise<T> implements IPromise {
+
+    public static class Trigger<U> {
+        private WeakReference<Promise<U>> parent;
+
+        private Trigger(Promise<U> parent) {
+            this.parent = new WeakReference<Promise<U>>(parent);
+        }
+
+        public void onSuccess(U u) {
+            Promise<U> p = this.parent.get();
+            if (p != null && p.isValid) {
+                synchronized (p.invalidationLock) {
+                    if (p.isValid) {
+                        p.onSuccessfulAction.run(u);
+                    }
+                }
+            }
+        }
+    }
+
+    public static class Builder {
+        static <U> Pair<Promise<U>, Trigger<U>> build() {
+            Promise<U> p = new Promise<>();
+
+            return new Pair<>(p, new Trigger<>(p));
+        }
+    }
+
+    private Object invalidationLock = new Object();
+
+    private Action<T> onSuccessfulAction;
+    private boolean   isValid;
+
+    private Promise() {
+        isValid = true;
+    }
+
+    public Promise<T> onSuccess(Action<T> action) {
+        onSuccessfulAction = action;
+        return this;
+    }
+
+    @Override
+    public void invalidate() {
+        synchronized (invalidationLock) {
+            isValid = false;
+        }
+    }
+}
